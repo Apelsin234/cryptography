a37d41d0a1d1a563bc317c03e63b3ef92331722e
Alexander Kravchenko <kravchenko99@yandex-team.ru>
2018-09-18 20:37:34 +0300

    add answer

--- homework1/src/main/java/ru/kravchenko99/cryptography/VigenereCipher.java
+++ homework1/src/main/java/ru/kravchenko99/cryptography/VigenereCipher.java
@@ -13,7 +13,7 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class VigenereCipher {
     private static final double[] LetterFrequency = {0.08167, 0.01492, 0.02782, 0.04253, 0.12702, 0.02228, 0.02015, 0.06094, 0.06966, 0.00153, 0.00772, 0.04025, 0.02406, 0.06749, 0.07507, 0.01929, 0.00095, 0.05987, 0.06327, 0.09056, 0.02758, 0.00978, 0.0236, 0.0015, 0.01975, 0.00074};
-    private static final String OUTPUT = "answer";
+    private static final String OUTPUT = "homework1/src/main/resources/output/answer";
 
     private static final double MATCH_INDEX = 0.055;
 
